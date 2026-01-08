36d78ad5f6fd1968f261da42b7e202f2e026d824
unknown <pavanadate4@gmail.com>
2026-01-08 03:23:33 -0800

    vv

---
+++ PropertySalesMVC/Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PropertySalesMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpPost]
+        public IActionResult Login(string adminId, string password)
+        {
+            // TEMP login (for testing)
+            if (adminId == "admin" && password == "1234")
+            {
+
+                HttpContext.Session.SetString("AdminLoggedIn", "true");
+                return Json(new { success = true });
+                
+            }
+
+            return Json(new
+            {
+                success = false,
+                message = "Invalid Admin ID or Password"
+            });
+        }
+
+
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult AddProperty()
+        {
+            return View();
+        }
+
+        public IActionResult AllProperties()
+        {
+            return View();
+        }    
+       
+
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
